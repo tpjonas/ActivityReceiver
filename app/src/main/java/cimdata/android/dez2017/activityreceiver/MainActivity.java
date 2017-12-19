b6bd94e068fd92161e1bc57a693bd2f18fc0a703
Jonas <tpjonas@gmail.com>
2017-12-19 09:30:44 +0100

    Initial commit

---
+++ app/src/main/java/cimdata/android/dez2017/activityreceiver/MainActivity.java
@@ -0,0 +1,13 @@
+package cimdata.android.dez2017.activityreceiver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
